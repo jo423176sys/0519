14cafe74e2f3492295f0f79fee032927f288600a
jo423176sys <jo423176@gmail.com>
2026-05-19 20:11:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,28 +6,20 @@ with st.sidebar:
     st.write("###  行事曆群組")
     st.radio("選擇群組", ["工作", "家庭"])
 
-col_left, col_right = st.columns([1, 2], gap="large")
+segmented_control
+    st.button("按鈕 A")
+    st.write("")  # 塞入一行空白間距
+    st.button("按鈕 B")
+
+col_left, col_right = st.columns([1, 1], gap="large")
 with col_left: 
     st.write("###  新增行程") 
-    txt=st.text_input("時間:09:00")
-    if st.button("新增行程"):
-        @st.dialog("新增完成")
-        def showadd():
-            st.write("新增成功")
-        showadd()
-    today = st.date_input("選擇日期", datetime.date.today())
-    meeting_time = st.time_input("選擇時間")
+    txt=st.text_input("行程主旨")
     my_color = st.color_picker("挑選辨識顏色","#1A73E8")
-
 
 with col_right: 
     st.write("###  設定區") 
-    tab1, tab2 = st.tabs(["本月行程", "已封存行程"])
-    with tab1: 
-        with st.container(border=True):
-             with st.container(border=True):
-                st.header("本月行程") 
-        with tab2: 
-            with st.container(border=True):
-                st.header("已封存行程") 
-        
+    today = st.date_input("選擇日期", datetime.date.today())
+    meeting_time = st.time_input("選擇時間")
+
+    
